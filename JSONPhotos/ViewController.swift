3e52f3352e21b493677f3e6cf549d293f897ae38
Anthony Merle <anthony.merle@webedia-group.com>
2022-02-20 22:17:32 +0100

    [Init] Create XCode project

---
+++ JSONPhotos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JSONPhotos
+//
+//  Created by Merle Anthony on 20/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
