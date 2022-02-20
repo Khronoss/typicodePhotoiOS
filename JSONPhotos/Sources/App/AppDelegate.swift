e5505fc6b1ea87d09b8741f5010a5b028f30ecc9
Anthony Merle <anthony.merle@webedia-group.com>
2022-02-20 23:12:18 +0100

    [Init] Setup project to be iOS 10 minum & setup basic folder architecture

---
+++ JSONPhotos/Sources/App/AppDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.swift
+//  JSONPhotos
+//
+//  Created by Merle Anthony on 20/02/2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        // Override point for customization after application launch.
+        self.window = Self.createWindow()
+        self.window?.makeKeyAndVisible()
+
+        return true
+    }
+
+    static func createWindow() -> UIWindow {
+        let window = UIWindow()
+
+        window.rootViewController = ViewController()
+
+        return window
+    }
+}
